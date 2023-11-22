fb05b0854690ecbca8b051f864ae798c81e7c1ff
rhysqi <rhysqi.akbar@gmail.com>
2023-11-23 02:24:46 +0700

    Adding window with terminal pop up function

---
+++ src/win32/window_com(pop).c
@@ -0,0 +1,47 @@
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+int main() {
+    const char *className = "SimpleWindowClass";
+
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = className;
+    RegisterClass(&wc);
+
+    // Create window
+    HWND hwnd = CreateWindowEx(
+        0,
+        className,
+        "Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, NULL, GetModuleHandle(NULL), NULL
+    );
+
+    if (hwnd == NULL) {
+        return 1;
+    }
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
